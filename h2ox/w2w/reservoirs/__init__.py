ac136bcba946c1f7818d988740b9a7e6c260fffc
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2022-05-04 22:22:02 +0000

    put imports back

---
+++ h2ox/w2w/reservoirs/__init__.py
@@ -0,0 +1,4 @@
+from h2ox.reservoirs.h2ox_data_clients import BQClient, WRISClient
+from h2ox.reservoirs.post_inference import BQInfClient, post_inference
+
+__all__ = ["WRISClient", "BQClient", "post_inference", "BQInfClient"]
